eb432ab38c9702c86608402dd193802c55b304e5
levi9111 <captainlevi9111@gmail.com>
2025-11-19 17:57:27 +0600

    authApi created

---
+++ src/redux/api/authApi.ts
@@ -0,0 +1,56 @@
+'use client';
+
+import { baseApi } from './baseApi';
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (credentials) => ({
+        url: '/auth/register',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: '/auth/login',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+
+    logout: builder.mutation({
+      query: () => ({
+        url: '/auth/logout',
+        method: 'POST',
+      }),
+    }),
+    getMe: builder.query({
+      query: () => '/auth/me',
+    }),
+
+    generateOtp: builder.mutation({
+      query: (email) => ({
+        url: '/auth/generate-otp',
+        method: 'POST',
+        body: email,
+      }),
+    }),
+    verifyOtp: builder.mutation({
+      query: (credentials) => ({
+        url: '/auth/verify-otp',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const {
+  useGetMeQuery,
+  useRegisterMutation,
+  useLoginMutation,
+  useGenerateOtpMutation,
+  useVerifyOtpMutation,
+} = authApi;
